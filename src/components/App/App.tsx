1579d9aa6c91ff7562d8e4b75dfb959f2dd80568
Volodymyr Burmitskyi <vburmtsk@gmail.com>
2026-09-17 15:47:55 -0700

    chore: initialize NoteHub with typed API and query provider

---
+++ src/components/App/App.tsx
@@ -0,0 +1,9 @@
+import css from './App.module.css';
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>NoteHub</header>
+    </div>
+  );
+}
